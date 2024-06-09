0c4cf1da6946e75b7eb3e4ca7e31c83d67467199
fievski <fievski@yandex.ru>
2024-06-09 23:46:36 +0600

    Add files via upload

---
+++ module1hard.py
@@ -0,0 +1,14 @@
+#grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+#students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+school_book = {'Aaron' : (5, 3, 3, 5, 4), 'Bilbo' : (2, 2, 2, 3), 'Johnny' : (4, 5, 5, 2), 'Khendrik' : (4, 4, 3), 'Steve' : (5, 5, 5, 4, 5)}
+a = ((5 + 3 + 3 + 5 + 4) / 5)
+b = ((2 + 2 + 2 + 3) / 4)
+c = ((4 + 5 + 5 + 2) / 4)
+d = ((4 + 4 + 3) / 3)
+e = ((5 + 5 + 5 + 4 + 5) / 5)
+school_book['Aaron'] = a
+school_book['Bilbo'] = b
+school_book['Johnny'] = c
+school_book['Khendrik'] = d
+school_book['Steve'] = e
+print(school_book)
